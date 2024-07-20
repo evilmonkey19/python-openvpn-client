d6b43e24af20f54d6bac903c586bacbf20b36594
BeethovenKodar <ludvig19@icloud.com>
2024-07-20 18:39:46 +0200

    fix ruff errors

--- openvpnclient.py
+++ openvpnclient.py
@@ -1,3 +1,5 @@
+"""OpenVPN client module."""
+
 from __future__ import annotations
 
 import logging
@@ -6,9 +8,12 @@ import signal
 import sys
 import threading
 from enum import Enum
+from pathlib import Path
 from subprocess import PIPE, Popen
 from tempfile import gettempdir
 from threading import Lock
+from types import TracebackType  # noqa: TCH003, used to type annotate
+from typing import Self
 
 import psutil
 from docopt import docopt
@@ -28,6 +33,8 @@ PID_FILE = f"{gettempdir()}/openvpnclient.pid"
 
 
 class Status(Enum):
+    """Status codes for the OpenVPN client."""
+
     CONNECTED = (1,)
     DISCONNECTED = (2,)
     CONNECTION_FAILED = (3,)
@@ -45,60 +52,69 @@ class OpenVPNClient:
     stdout: str = ""
 
     def __init__(self, ovpn_file: str, connect_timeout: int = 5) -> None:
-        """Initialize the OpenVPN client
+        """Initialize the OpenVPN client.
 
         Args:
+        ----
             ovpn_file (str): The OpenVPN configuration file
             connect_timeout (int): The connection attempt limit in seconds
 
         Raises:
+        ------
             ValueError: If connect_timeout is less than, or equal to, 0
+
         """
         if connect_timeout <= 0:
-            raise ValueError("Connection timeout must be at least 1 second")
+            err_msg = "Connection timeout must be at least 1 second"
+            raise ValueError(err_msg)
         self.connect_timeout = connect_timeout
-        self.ovpn_dir = os.path.dirname(ovpn_file)
+        self.ovpn_dir = Path(ovpn_file).parent
         self.ovpn_file = ovpn_file
 
-    def __enter__(self) -> OpenVPNClient:
+    def __enter__(self) -> Self:
+        """Connect to the OpenVPN server when entering a context manager."""
         self.connect()
         return self
 
-    def __exit__(self, *_) -> None:
+    def __exit__(self, exc_type, exc_value, traceback) -> None:  # noqa: ANN001, unused but needed for magic method
+        """Disconnect from the OpenVPN server when exiting a context manager."""
         self.disconnect()
 
-    def connect(self, stay_alive_on_exit: bool = False) -> None:
-        """Connect to the OpenVPN server using the provided configuration file
+    def connect(self, *, stay_alive_on_exit: bool = False) -> None:  # noqa: PLR0915, C901, hard to factor out cleanly
+        """Connect to the OpenVPN server using the provided configuration file.
 
         Args:
+        ----
             stay_alive_on_exit (bool): If True, the connection will not be
             terminated when the script is exited
 
         Raises:
+        ------
             NotADirectoryError: If the configuration file's directory is not found
             FileNotFoundError: If the configuration file is not found
             ConnectionError: If the client is already connected
             TimeoutError: If the connection attempt times out
+
         """
-        if not os.path.exists(self.ovpn_dir):
-            raise NotADirectoryError(f"Directory '{self.ovpn_dir}' not found")
-        if not os.path.exists(self.ovpn_file):
-            raise FileNotFoundError(f"File '{self.ovpn_file}' not found")
+        if not Path(self.ovpn_file).exists():
+            err_msg = f"File '{self.ovpn_file}' not found"
+            raise FileNotFoundError(err_msg)
         if OpenVPNClient._get_pid() != -1:
-            raise ConnectionError("Already connected")
+            err_msg = "Already connected"
+            raise ConnectionError(err_msg)
 
         lock = Lock()
         lock.acquire()
 
-        def on_connected(*_) -> None:
+        def on_connected(*_) -> None:  # noqa: ANN002, not relevant for functionality
             self.status = Status.CONNECTED
             lock.release()
 
-        def on_connect_timeout(*_) -> None:
+        def on_connect_timeout(*_) -> None:  # noqa: ANN002, not relevant for functionality
             self.status = Status.CONNECTION_TIMEOUT
             lock.release()
 
-        def on_user_cancelled(*_) -> None:
+        def on_user_cancelled(*_) -> None:  # noqa: ANN002, not relevant for functionality
             if self.status is Status.CONNECTED:
                 OpenVPNClient.disconnect()
             else:
@@ -111,15 +127,24 @@ class OpenVPNClient:
         timer.start()
 
         cmd = [
-            "sudo", "openvpn",
-            "--cd", self.ovpn_dir,
-            "--config", self.ovpn_file,
-            "--dev", "tun_ovpn",
-            "--connect-retry-max", "3",
-            "--connect-timeout", str(self.connect_timeout),
-            "--script-security", "2",
-            "--route-delay", "1",
-            "--route-up", f"{sys.executable} -c 'import os, signal; os.kill({os.getpid()}, signal.SIGUSR1)'"
+            "sudo",
+            "openvpn",
+            "--cd",
+            self.ovpn_dir,
+            "--config",
+            self.ovpn_file,
+            "--dev",
+            "tun_ovpn",
+            "--connect-retry-max",
+            "3",
+            "--connect-timeout",
+            str(self.connect_timeout),
+            "--script-security",
+            "2",
+            "--route-delay",
+            "1",
+            "--route-up",
+            f"{sys.executable} -c 'import os, signal; os.kill({os.getpid()}, signal.SIGUSR1)'",
         ]
         self.proc = Popen(cmd, stderr=PIPE, stdout=PIPE)
 
@@ -143,15 +168,14 @@ class OpenVPNClient:
                         \rstderr: {stderr}
                     """
                     raise ConnectionRefusedError(msg)
-                else:
-                    self.status = Status.DISCONNECTED
+                self.status = Status.DISCONNECTED
 
             self.on_exit_thread = threading.Thread(target=proc_exited)
             self.on_exit_thread.start()
 
-            def excepthook(args) -> None:
+            def excepthook(args: tuple[type, BaseException, TracebackType]) -> None:
                 lock.release()
-                raise args.exc_type(args.exc_value)
+                raise args[0](args[1])
 
             threading.excepthook = lambda args: excepthook(args)
 
@@ -162,64 +186,72 @@ class OpenVPNClient:
                 logger.info("OpenVPN connection successful")
             elif self.status is Status.CONNECTION_TIMEOUT:
                 OpenVPNClient.disconnect()
-                raise TimeoutError(f"Did not connect in {self.connect_timeout}s")
+                err_msg = f"Did not connect in {self.connect_timeout}s"
+                raise TimeoutError(err_msg)
             elif self.status is Status.USER_CANCELLED:
                 OpenVPNClient.disconnect()
             elif self.status is Status.CONNECTION_FAILED:
                 OpenVPNClient._remove_pid_file()
-                raise ConnectionRefusedError("OpenVPN connection failed")
+                err_msg = "OpenVPN connection failed"
+                raise ConnectionRefusedError(err_msg)
 
     @staticmethod
     def _register_pid(pid: int) -> None:
-        """Store the PID of the active OpenVPN process
+        """Store the PID of the active OpenVPN process.
 
         Args:
+        ----
             pid (int): The process ID
+
         """
-        with open(PID_FILE, "w") as f:
+        with Path(PID_FILE).open("w") as f:
             f.write(str(pid))
 
     @staticmethod
     def _get_pid() -> int:
-        """Retrieve the PID of the active OpenVPN process
+        """Retrieve the PID of the active OpenVPN process.
 
-        Returns:
+        Return:
+        ------
             int: The process ID
+
         """
         try:
-            with open(PID_FILE, "r") as f:
+            with Path(PID_FILE).open() as f:
                 try:
-                    pid = int(f.read().strip())
-                    return pid
-                except ValueError as e:
-                    logger.error(f"PID in '{PID_FILE}' is not an integer")
-                    raise e
+                    return int(f.read().strip())
+                except ValueError:
+                    err_msg = f"PID in '{PID_FILE}' is not an integer"
+                    logger.exception(err_msg)
+                    raise
         except FileNotFoundError:
             return -1
 
     @staticmethod
     def _remove_pid_file() -> None:
-        """Remove the PID file"""
-        os.remove(PID_FILE)
+        """Remove the PID file."""
+        Path(PID_FILE).unlink()
 
     @staticmethod
     def disconnect() -> None:
-        """Disconnect the current OpenVPN connection
+        """Disconnect the current OpenVPN connection.
 
-        Raises:
+        Raise:
+        ------
             ProcessLookupError: If the PID file is not found or the PID file is corrupt
             TimeoutError: If the process doesn't terminate in 5 seconds
+
         """
         pid = OpenVPNClient._get_pid()
         if pid == -1:
-            raise ProcessLookupError("Did not find an ongoing connection")
+            err_msg = "No ongoing connection found"
+            raise ProcessLookupError(err_msg)
         try:
             proc = psutil.Process(pid)
         except psutil.NoSuchProcess:
             OpenVPNClient._remove_pid_file()
-            raise ProcessLookupError(
-                f"Corrupt PID file, PID {pid} doesn't exist, removed file"
-            )
+            err_msg = f"Corrupt PID file, PID {pid} doesn't exist, removed file"
+            raise ProcessLookupError(err_msg) from None
         proc.terminate()  # 'explicit-exit-notify' requires SIGTERM
         timeout = 5
         try:
@@ -228,7 +260,8 @@ class OpenVPNClient:
             logger.info("Process terminated")
         except TimeoutError:  # unable to force slower termination
             proc.kill()
-            raise TimeoutError(f"Process didn't terminate in {timeout}, killed instead")
+            err_msg = f"Process didn't terminate in {timeout}, killed instead"
+            raise TimeoutError(err_msg) from None
 
 
 usage = """
@@ -250,5 +283,5 @@ if __name__ == "__main__":
         config_file = args["--config"]
         OpenVPNClient(config_file).connect(stay_alive_on_exit=True)
     else:
-        print(usage)
+        print(usage)  # noqa: T201, print when invoked as script
         sys.exit(1)
